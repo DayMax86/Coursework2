38e6bbaac8f01235be3a5caf3c29472aef48962a
DayMax86 <daymax96@gmail.com>
2023-11-30 17:21:35 +0000

    SortedLinkedList skeleton

---
+++ src/SortedLinkedList.java
@@ -0,0 +1,197 @@
+public class SortedLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+
+/**
+ * SortedList interface for use with the CM10228: Principles of Programming 2 coursework.
+ *
+ * This should not be modified by the student.
+ *
+ * @author		Christopher Clarke
+ * @version		1.0
+ */
+interface SortedList {
+
+    /**
+     * Returns the number of Nodes in the linked list.
+     *
+     * @return      the number of Nodes in the linked list
+     */
+    public int size();
+
+    /**
+     * Adds a Node with the specified string to the linked list in
+     * the appropriate position given the specified alphabetical order
+     * (i.e., ascending/descending).
+     *
+     * @param  string  a String to be added to the linked list
+     */
+    public void add(String string);
+
+    /**
+     * Adds a Node to the linked list in the appropriate position
+     * given the specified alphabetical order (i.e., ascending/descending).
+     *
+     * @param  node  a Node to be added to the linked list
+     */
+    public void add(Node node);
+
+    /**
+     * Returns the first Node of the linked list given the specified
+     * alphabetical order (i.e., ascending/descending).
+     *
+     * @return      the first Node in the linked list
+     */
+    public Node getFirst();
+
+    /**
+     * Returns the last Node of the linked list given the specified
+     * alphabetical order (i.e., ascending/descending).
+     *
+     * @return      the last Node in the linked list
+     */
+    public Node getLast();
+
+    /**
+     * Returns the Node at the specified index assuming indices start
+     * at 0 and end with size-1 given the specified alphabetical order
+     * (i.e., ascending/descending).
+     *
+     * @param  index  the index of the Node in the linked list to be retrieved
+     * @return      the Node in the linked list at the specified index
+     */
+    public Node get(int index);
+
+    /**
+     * Checks to see if the list contains a Node with the specified
+     * string.
+     *
+     * @param  string  the String to be searched for in the linked list
+     * @return       True if the string is present or false if not
+     */
+    public boolean isPresent(String string);
+
+    /**
+     * Removes the first Node from the list given the specified
+     * alphabetical order (i.e., ascending/descending).
+     *
+     * @return      Returns true if successful or false if unsuccessful
+     */
+    public boolean removeFirst();
+
+    /**
+     * Removes the last Node from the list given the specified
+     * alphabetical order (i.e., ascending/descending).
+     *
+     * @return      Returns true if successful or false if unsuccessful
+     */
+    public boolean removeLast();
+
+    /**
+     * Removes the Node at the specified index from the list assuming indices
+     * start at 0 and end with size-1 given the specified alphabetical order
+     * (i.e., ascending/descending)
+     *
+     * @param  index  the index of the Node in the linked list to be removed
+     * @return      Returns true if successful or false if unsuccessful
+     */
+    public boolean remove(int index);
+
+    /**
+     * Removes the Node from the list that contains the specified string.
+     *
+     * @param  string  the string to be removed from the linked list
+     * @return      Returns true if successful or false if unsuccessful
+     */
+    public boolean remove(String string);
+
+    /**
+     * Orders the linked list in ascending alphabetical order.
+     *
+     */
+    public void orderAscending();
+
+    /**
+     * Orders the linked list in descending alphabetical order.
+     *
+     */
+    public void orderDescending();
+
+    /**
+     * Prints the contents of the linked list in the specified alphabetical order
+     * (i.e., ascending/descending) to System.out with each node's string on
+     * a new line.
+     *
+     */
+    public void print();
+}
+
+
+/**
+ * Node class for use with the CM10228: Principles of Programming 2 coursework.
+ *
+ * This should not be modified by the student.
+ *
+ * @author		Christopher Clarke
+ * @version		1.0
+ */
+class Node {
+    private String name;
+    private Node prev;
+    private Node next;
+
+    public Node(String name) {
+        this.prev = null;
+        this.name = name;
+        this.next = null;
+    }
+
+    public Node(String name, Node next) {
+        this.prev = null;
+        this.name = name;
+        this.next = next;
+    }
+
+    public Node(Node prev, String name) {
+        this.prev = prev;
+        this.name = name;
+        this.next = null;
+    }
+
+    public Node(Node prev, String name, Node next) {
+        this.prev = prev;
+        this.name = name;
+        this.next = next;
+    }
+
+    public void setString(String name) {
+        this.name = name;
+    }
+
+    public String getString() {
+        return this.name;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getPrev() {
+        return this.prev;
+    }
+}
